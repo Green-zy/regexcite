1a7d9fc6e8bbc56883fb73d99d04b032dee6a02a
Green-zy <zhou165@usc.edu>
2025-01-10 15:48:08 -0800

    added documentation for strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -2,6 +2,17 @@
 # The link is: https://r-pkgs.org/whole-game.html
 # The code is used to build a toy function to practice how to package in R
 
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
